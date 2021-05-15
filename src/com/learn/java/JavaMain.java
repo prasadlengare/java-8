531aa2dede99c8a353e7b7ca7ee95530dabe8bcc
Prasad Lengare <prasadlengare@gmail.com>
2021-05-15 19:53:30 +0530

    initial commit

---
+++ src/com/learn/java/JavaMain.java
@@ -0,0 +1,7 @@
+package com.learn.java;
+
+public class JavaMain {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/com/learn/java/lambdas/RunnableLambdaExample.java
@@ -0,0 +1,33 @@
+package com.learn.java.lambdas;
+
+public class RunnableLambdaExample {
+    public static void main(String[] args) {
+
+        /*
+        Runnable before java 8
+        */
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Runnable ");
+            }
+        };
+        new Thread(runnable).start();
+
+        /*
+        Runnable after java 8
+        */
+        //Method 1
+        Runnable runnable1 = ()->{
+            System.out.println("Runnable 1");
+        };
+        new Thread(runnable1).start();
+
+        //Method 2
+        Runnable runnable2 = ()->System.out.println("Runnable 2");
+        new Thread(runnable2).start();
+
+        //Method 3
+        new Thread(()->System.out.println("Runnable 3")).start();
+    }
+}
