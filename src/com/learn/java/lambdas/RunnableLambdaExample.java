6fd8af68f5b3ed649c8794c778f0fb5130fb6f3c
Prasad Lengare <prasadlengare@gmail.com>
2024-02-02 21:57:58 +0530

    formatting added

--- src/com/learn/java/lambdas/RunnableLambdaExample.java
+++ src/com/learn/java/lambdas/RunnableLambdaExample.java
@@ -18,16 +18,16 @@ public class RunnableLambdaExample {
         Runnable after java 8
         */
         //Method 1
-        Runnable runnable1 = ()->{
+        Runnable runnable1 = () -> {
             System.out.println("Runnable 1");
         };
         new Thread(runnable1).start();
 
         //Method 2
-        Runnable runnable2 = ()->System.out.println("Runnable 2");
+        Runnable runnable2 = () -> System.out.println("Runnable 2");
         new Thread(runnable2).start();
 
         //Method 3
-        new Thread(()->System.out.println("Runnable 3")).start();
+        new Thread(() -> System.out.println("Runnable 3")).start();
     }
 }
